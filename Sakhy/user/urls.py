6110abcde0b318fdfd7fe9f5a54b52d00b56efc0
AMozy <a.mozyen@gmail.com>
2023-12-03 14:01:26 +0300

    remove app

--- Sakhy/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Register, Login, UserDetails, Logout
-
-urlpatterns = [
-    path("register/", Register.as_view(), name="register"),
-    path("login/", Login.as_view(), name="login"),
-    path("user_details/", UserDetails.as_view(), name="user details"),
-    path("logout/", Logout.as_view(), name="logout"),
-]
